cb20685fdd248ffa42cc419a5dbf3c60d44bbaa8
robertsondias <robertsondias@gmail.com>
2023-08-23 11:12:54 -0300

    Update ArraysComFor.java

--- src/main/java/com/example/AppArraysComFor.java
+++ src/main/java/com/example/AppArraysComFor.java
@@ -4,27 +4,28 @@ import java.util.Arrays;
 
 public class AppArraysComFor {
     public static void main(String[] args) {
+        
         int vetor3[] = new int[5];
-        for (int i = 0; i < vetor3.length; i++) {
-            vetor3[i] = 100 * (i+1);            
+        for (int i=0; i < 5; i++) {
+            vetor3[i] = 100 * (i+1);
         }
         System.out.println(Arrays.toString(vetor3));
         System.out.println();
 
-        double matriz [][] = new double [3][4];
+        double matriz[][] = new double[3][4];
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[1].length; j++) {
-                matriz[i][j] = (i * matriz[i].length + j) * 10;                
-            }       
-        }
-        
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[1].length; j++) {
-                System.out.printf("%6.1f", matriz[i][j]);  
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (i * matriz[i].length + j+1) * 10;
             }
-            System.err.println();              
         }
 
-       
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%6.1f", matriz[i][j]);
+                
+            }
+            System.out.println();
+            
+        }
     }
 }
